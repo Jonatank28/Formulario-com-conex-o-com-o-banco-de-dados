cbebca478a978333639385426a9cf7b8c19376e9
Jonatank28 <jonatankalmeidakk28@gmail.com>
2023-01-14 21:37:18 -0300

    conexão com o banco configurada

--- server/index.js
+++ server/index.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const app = express();
-const mysql = express();
+const mysql = require("mysql");
 
-const db = mysql.createPool({
+const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: '',
+    database: 'login',
 })
 
+app.get("/", (req, res) => {
+    db.query("INSERT INTO usuario (email, password) VALUES ('jonatankalmeidakk28@gmail.com', '1234567890') ", (err, result) => {
+        if(err){
+            console.log(err);
+        }
+        res.send(result)
+    })
+})
 
-app.listen(3001, () => {
-    console.log("Server rodando na porta 3001")
+app.listen(3005, (req, res) => {
+    console.log("Server rodando na porta 3005")
 } )
+
